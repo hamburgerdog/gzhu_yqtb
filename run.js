42e6c23acf1430afe7c4fee82906db1ec7882390
hamburgerdog <838980000@qq.com>
2021-09-20 20:55:32 +0800

    :zap:【完善】添加lock文件｜完善脚本

--- run.js
+++ run.js
@@ -1,16 +1,21 @@
 const puppeteer = require('puppeteer');
 
 const infos = process.argv.slice(2);
-if (infos.length !== 2) throw new Error('输入信息不正确，请重新输入格式为： [id] [password]');
-if (!infos[0].match(/^\d{10}$/)) throw new Error('ID格式不正确');
-if (infos[1].length < 6 || infos[1].length > 18) throw new Error('密码格式不正确');
-const [id, password] = infos;
+if (infos.length % 2 !== 0) throw new Error('输入信息不正确，请重新输入格式为： [id] [password]');
+
+infos.forEach((item, index) => {
+  if (index % 2 === 0) {
+    if (!item.match(/^\d{10}$/)) throw new Error('ID格式不正确');
+  } else {
+    if (item.length < 6 || item.length > 18) throw new Error('密码格式不正确');
+  }
+});
 
 const PAGE_TIMEOUT = 60 * 1000;
 const COMMON_TIMEOUT = 5 * 1000;
 const LOGIN_PATH = "http://yqtb.gzhu.edu.cn/infoplus/form/XNYQSB/start?back=1&x_posted=true";
 
-(async () => {
+const login = async (id, password) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   page.setDefaultTimeout(PAGE_TIMEOUT);
@@ -36,4 +41,8 @@ const LOGIN_PATH = "http://yqtb.gzhu.edu.cn/infoplus/form/XNYQSB/start?back=1&x_
   await page.click("#V1_CTRL82");
   await Promise.all([page.click(".command_button_content"), page.waitForTimeout(COMMON_TIMEOUT * 2)]);
   browser.close();
-})();
+};
+
+for (let i = 0; i < infos.length; i += 2) {
+  login(infos[i], infos[i + 1]);
+};
